d8f7036c129b8e24e28e1442b348219a007ea739
Andrey Ogarev <oandreiy@gmail.com>
2013-09-15 19:48:07 +0000

    Whith content from requestHandlers.js

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 #!usr/bin/env node
 
 function route(handle,pathname) {
-    console.log("about to route a request for " + pathname);
+    console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-	handle[pathname]();
+	return handle[pathname]();
    } else {
        console.log("No request handler found for" + pathname);
+       return "404 Not Found";
   }
     
 }
