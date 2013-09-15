72be9c2f911578c3b589c8c25c20a46f057663eb
Andrey Ogarev <oandreiy@gmail.com>
2013-09-15 21:49:40 +0000

    Add Asinhronius

--- router.js
+++ router.js
@@ -1,12 +1,15 @@
 #!usr/bin/env node
 
-function route(handle,pathname) {
+function route(handle,pathname,response) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-	return handle[pathname]();
+	handle[pathname](response);
    } else {
        console.log("No request handler found for" + pathname);
-       return "404 Not Found";
+       response.writeHead(404, {"Content-Type":"text/plain"});
+       response.write("404 Not found");
+       response.end();
   }
+    
 }
 exports.route=route;
