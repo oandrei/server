34c240aa9483bf3bf3ad96fc1820b9bdfbd08d21
Andrey Ogarev <oandreiy@gmail.com>
2013-09-15 19:45:35 +0000

    With sleep function

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,20 @@
 #!usr/bin/env node
 
+var exec=require("child_process").exec;
+
 function start() {
     console.log("Request handler 'start' was called");
+	
+    function sleep (milliSeconds){
+	var startTime = new Date().getTime();
+	while (new Date().getTime()< startTime + milliSeconds);
+    }
+    sleep(10000);
+    return "Hello Start";
 }
 function upload() {
     console.log("Request handler 'upload' was called");
+    return "Hello Upload";
 }
 
 exports.start=start;
