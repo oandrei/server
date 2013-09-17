f3d1a770520a4391dee78552ce939a553a500dae
Andrey Ogarev <oandreiy@gmail.com>
2013-09-17 01:51:20 +0000

    Add show handle

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 var server=require("./server");
 var router=require("./router");
-var requestHandlers=require("./requestHandlers");
+var requestHandlers=require("./requestHandlers1");
 
 var handle={};
 handle["/"]=requestHandlers.start;
 handle["/start"]=requestHandlers.start; 
 handle["/upload"]=requestHandlers.upload; 
+handle["/show"]=requestHandlers.show;
 
 server.start(router.route,handle);
